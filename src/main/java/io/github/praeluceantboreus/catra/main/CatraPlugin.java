6edfb1c00d84bfcee80060df9bc074f1d765804f
pubmem <richard.stoeckl@aon.at>
2015-07-19 16:07:37 +0200

    added world config

--- src/main/java/io/github/praeluceantboreus/catra/main/CatraPlugin.java
+++ src/main/java/io/github/praeluceantboreus/catra/main/CatraPlugin.java
@@ -1,17 +1,35 @@
 package io.github.praeluceantboreus.catra.main;
 
+import io.github.praeluceantboreus.catra.serialize.Coords;
+
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CatraPlugin extends JavaPlugin
 {
+	@Override
+	public void onEnable()
+	{
+		genConfig();
+		System.out.println(Coords.deserialize(getConfig().getConfigurationSection("worlds"), getServer()));
+		super.onEnable();
+	}
+
 	public void genConfig()
 	{
+		Coords coords = new Coords();
+		for (World w : getServer().getWorlds())
+		{
+			coords.addWorld(new Location(w, 0, 0, 0), new Location(w, 1, 2, 3));
+		}
+		getConfig().addDefault("worlds", coords.serialize());
 		getConfig().addDefault("trader.amount", 2);
 		GregorianCalendar gc = new GregorianCalendar();
 		gc.setTime(new Date(0));
